18b6ad7f52e94bded2f814870ed052adfdabc932
fenil <fenilp2046@gmail.com>
2024-10-02 20:06:55 +0530

    thoughtful

---
+++ interesting/pascals triangle.py
@@ -0,0 +1,13 @@
+num=int(input("Enter number of rows : "))
+list=[]
+
+for i in range(num):
+    temp_list=[]
+    for j in range(i+1):
+        if j==0 or i==j:
+            temp_list.append(1)
+        else:
+            temp_list.append(list[i-1][j-1]+list[i-1][j])
+    list.append(temp_list)
+    print(temp_list)
+    
